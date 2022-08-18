b27a4c14936bb4fe1d5815b84e12c78f71ce331f
DmitriySolenkov <soledimk@gmail.com>
2022-08-18 17:33:48 +0300

    Ex26

---
+++ Ex26/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите ваше число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int res=1;
+while(num>10)
+{
+num=num/10;
+res++;
+}
+
+Console.Write($"Количество символов в числе: {res}");
