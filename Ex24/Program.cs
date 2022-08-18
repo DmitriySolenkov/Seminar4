34645b4f6d390ed111fd7d1bf5808d9f9f9811c5
DmitriySolenkov <soledimk@gmail.com>
2022-08-18 16:43:24 +0300

    Ex24

---
+++ Ex24/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Enter your number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int result=0;
+for (int i = 1; i <= num; i++)
+{
+    result=result+i;
+}
+Console.Write(result);
