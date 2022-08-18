2c7d15d99a2686aebe27bfae2d2b9d74bcb929f3
DmitriySolenkov <soledimk@gmail.com>
2022-08-18 18:16:56 +0300

    Ex30

---
+++ Ex30/Program.cs
@@ -0,0 +1,10 @@
+﻿int[] array= new int[8];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i]= new Random().Next(0,2);
+}
+
+for (int k = 0; k < array.Length; k++)
+{
+    Console.Write(array[k]+ ", ");
+}
