2f297098ad25eb0f8b6fec5f7b278bfe356b0702
DmitriySolenkov <soledimk@gmail.com>
2022-08-18 18:02:03 +0300

    Ex28

---
+++ Ex28/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите ваше число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int index=1;
+int res=1;
+while (index<=num)
+{
+res= res*index;
+index++;
+}
+Console.Write($"Произведение чисел от 1 до вашего числа: {res}");
